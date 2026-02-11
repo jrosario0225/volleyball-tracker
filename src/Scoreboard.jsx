004db8fb268b58767112995cce4d0e9074e073cf
mishareyes <hellomboreyes@gmail.com>
2026-02-11 13:34:56 -0800

    justify text

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -4,10 +4,19 @@ import './App.css'
 export function Scoreboard({hustleTotalScore, otherTotalScore}) {
     return (
         <div className="scoreboard">
-          <div className="team-name">Hustle</div>
-          <div className="total-score">{hustleTotalScore}</div>
-          <div className="total-score">{otherTotalScore}</div>
-          <div className="team-name">Other</div>
+            <div className="team-name justify-right">
+                <img src="/hustle-logo.jpeg" alt="Hustle logo" className="team-logo" />
+                <p>Hustle</p>
+            </div>
+            <div className="total-score">
+                <p>{hustleTotalScore}</p>
+            </div>
+            <div className="total-score">
+                <p>{otherTotalScore}</p>
+            </div>
+            <div className="team-name justify-left">
+                <p>Other</p>
+            </div>
       </div>
     )
 }
