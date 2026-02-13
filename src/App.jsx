2102334e05fed41b563bd2efaf7ac084452869cf
Jacob Rosario <jacobcvrosario@gmail.com>
2026-02-13 13:20:05 -0800

    completed TeamSeciton.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 import Scoreboard from './components/Scoreboard'
+import TeamSection from './components/TeamSection'
 
 function App() {
 
--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import "./TeamSection.css";
 
-function TeamSection() {
+function TeamSection({ earned, errors,
+    onAddEarned, onSubtractEarned,
+    onAddErrors, onSubtractErrors
+}) {
+    return (
+        <div className="team-section">
+            <div className="stat">
+                <div className="stat-item">
+                    <p>Points earned: {earned}</p>
+                </div>
+                <div className="stat-item">
+                    <p>Errors made: {errors}</p>
+                </div>
+            </div>
 
+            <div className="stat-buttons">
+                <div className="button-group">
+                    <button onClick={onSubtractEarned} className="button-subtract">-</button>
+                    <button onClick={onAddEarned} className="button-add">+</button>
+                </div>
+                <div className="button-group">
+                    <button onClick={onSubtractErrors} className="button-subtract">-</button>
+                    <button onClick={onAddErrors} className="button-add">+</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default TeamSection;
