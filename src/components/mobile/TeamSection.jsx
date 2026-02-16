8f39a4420c8a3b30aad4d20f5caa68a7fd00b1e1
Jacob Rosario <jacobcvrosario@gmail.com>
2026-02-16 04:56:49 -0800

    Finished mobile optimization

--- src/components/mobile/TeamSection.jsx
+++ src/components/mobile/TeamSection.jsx
@@ -2,31 +2,35 @@ import React from "react";
 import "./TeamSection.css";
 
 
-function TeamSection({teamName, 
-    earned, 
+function TeamSection({ teamName,
+    earned,
     errors,
     onAddEarned,
     onSubtractEarned,
     onAddErrors,
     onSubtractErrors
 }) {
-    return(
+    return (
         <div className="team-section-mobile">
             <div className="team-name-mobile">
                 <h2>{teamName}</h2>
             </div>
             <div className="stat-row-mobile">
-                <p>Earned</p>
-                <p>{earned}</p>
-                <button onClick={onSubtractEarned} className="button-subtract">-</button>
-                <button onClick={onAddEarned} className="button-add">+</button>
+                <p className="stat-label">Earned</p>
+                <p className="stat-number">{earned}</p>
+                <div className="button-group-mobile">
+                    <button onClick={onSubtractEarned} className="button-subtract">-</button>
+                    <button onClick={onAddEarned} className="button-add">+</button>
+                </div>
             </div>
 
             <div className="stat-row-mobile">
-                <p>Errors</p>
-                <p>{errors}</p>
-                <button onClick={onSubtractErrors} className="button-subtract">-</button>
-                <button onClick={onAddErrors} className="button-add">+</button>
+                <p className="stat-label">Errors</p>
+                <p className="stat-number">{errors}</p>
+                <div className="button-group-mobile">
+                    <button onClick={onSubtractErrors} className="button-subtract">-</button>
+                    <button onClick={onAddErrors} className="button-add">+</button>
+                </div>
             </div>
         </div>
     )
