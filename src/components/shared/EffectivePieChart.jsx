e6fabbd1093aa7fef95e3aa885ba9e1a3cac2cbc
Jacob Rosario <jacobcvrosario@gmail.com>
2026-03-01 15:12:32 -0800

    added effectiveness charts

--- src/components/shared/EffectivePieChart.jsx
+++ src/components/shared/EffectivePieChart.jsx
@@ -11,16 +11,37 @@ function EffectivePieChart({earned, errors}) {
 
     const COLORS = ["#2ecc71", "#e74c3c"]
 
+    // adding labels to pie chart
+
+    const renderCustomLabel = ({ cx, cy, midAngle, outerRadius, value, name }) => {
+        const RADIAN = Math.PI / 180
+        const radius = outerRadius + 40
+        const x = cx + radius * Math.cos(-midAngle * RADIAN)
+        const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central" fontSize={10}>
+                {`${name}: ${value}%`}
+            </text>
+        )
+
+
+
+    }
+
     return (
-        <div className="pie-chart-container">
-            <PieChart width={220} height={220}>
+        <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <h3 style={{ alignSelf: "center", marginBottom: "0"}}>Effectiveness</h3>
+            <PieChart width={500} height={300}>
                 <Pie
                 data={data}
-                cx={105}
-                cy={100}
+                cx={250}
+                cy={140}
                 innerRadius={55}
                 outerRadius={80}
                 dataKey="value"
+                label={renderCustomLabel}
+                labelLine={true}
                 >
 
                     {data.map((entry, index) => (
@@ -28,7 +49,6 @@ function EffectivePieChart({earned, errors}) {
                     ))}
                 </Pie>
                 <Tooltip formatter={(value) => `${value}%`} />
-                <Legend />
             </PieChart>
         </div>
     )
---
+++ src/components/shared/StatsPieChart.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { PieChart, Pie, Cell, Tooltip } from "recharts"
+
+const statLabels = {
+
+}
+
+function StatsPieChart() {
+
+}
+
+export default StatsPieChart
